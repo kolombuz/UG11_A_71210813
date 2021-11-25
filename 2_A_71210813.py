73ce842c7dcda28161badbe217ba83c25c2f8a6a
Billie Steven <billie.steven@ti.ukdw.ac.id>
2021-11-26 06:39:44 +0700

    SOAL 2

---
+++ 2_A_71210813.py
@@ -0,0 +1,12 @@
+def huruf_e():
+    karakter = "abcdefg"
+    print(karakter[4])
+huruf_e()
+def huruf_f():
+    karakter = "abcdefg"
+    print(karakter[5])
+huruf_f()
+def huruf_c():
+    karakter = "abcd"
+    print(karakter[2])
+huruf_c()
